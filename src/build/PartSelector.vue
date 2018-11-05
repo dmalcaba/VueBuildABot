8f9568dbc400e07dd207765b9547d69bb6d3e3c2
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-11-05 15:57:46 -0800

    Pass data from child component to parent via emit

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -47,14 +47,15 @@ export default {
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.$emit('partSelected', this.selectedPart);
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.$emit('partSelected', this.selectedPart);
     },
-
   },
 };
 
