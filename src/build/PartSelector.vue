6d7d1d7f5011a6de5ff1bd29194c07133a47e67d
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-11-06 17:23:43 -0800

    Add the click event to link to the PartInfo comp

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="arm"/>
+    <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -63,6 +63,9 @@ export default {
         this.parts.length,
       );
     },
+    showPartInfo() {
+      this.$router.push('/parts');
+    },
   },
 };
 
@@ -94,6 +97,7 @@ export default {
 }
 .part img {
   width:165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
