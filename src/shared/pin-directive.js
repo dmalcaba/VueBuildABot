dda554f7cfdaa90bfb9553d18c0d6c1ee69ba2fc
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-11-07 16:49:28 -0800

    Pass value as an object to the directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-param-reassign */
 export default {
   bind: (element, binding) => {
-    if (binding.arg !== 'position') return;
-
-    Object.keys(binding.modifiers).forEach((key) => {
-      element.style[key] = '5px';
+    Object.keys(binding.value).forEach((position) => {
+      element.style[position] = binding.value[position];
     });
     element.style.position = 'absolute';
   },
