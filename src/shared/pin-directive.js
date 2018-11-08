3fb831cfd0fbe2bfd0fcfe336d422a8f41b56702
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-11-07 17:04:32 -0800

    Use shorthand method for bind and update
    This is the common way of creating directives

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,17 +1,7 @@
 /* eslint-disable no-param-reassign */
-
-function applyStyle(element, binding) {
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-
-export default {
-  bind: (element, binding) => {
-    applyStyle(element, binding);
-  },
-  update: (element, binding) => {
-    applyStyle(element, binding);
-  },
-};
