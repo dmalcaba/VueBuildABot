09ec9af9794c962a70ca922560fce7ecde9b0090
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-11-07 17:00:17 -0800

    Made sure that changes are done to styles on update
    This demonstrates the lifecycle hooks for directives.

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="part" :class="position">
+    {{pinPadding}}
     <router-link :to="{
         name: 'Parts',
         params: {
@@ -11,7 +12,11 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin="{ bottom: '10px', right: '5px' }" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span
+      @click="pinPadding='30px'"
+      v-pin="{ bottom: pinPadding, right: pinPadding }" class="sale" v-show="selectedPart.onSale">
+      Sale!
+    </span>
   </div>
 </template>
 
@@ -52,7 +57,10 @@ export default {
     this.emitPartSelected();
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-param-reassign */
+
+function applyStyle(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+}
+
 export default {
   bind: (element, binding) => {
-    Object.keys(binding.value).forEach((position) => {
-      element.style[position] = binding.value[position];
-    });
-    element.style.position = 'absolute';
+    applyStyle(element, binding);
+  },
+  update: (element, binding) => {
+    applyStyle(element, binding);
   },
 };
